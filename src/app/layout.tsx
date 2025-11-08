63b893b426973e34bd76271cd7460528bb97e464
Handy Hasan <handy.hasan@yahoo.com>
2025-11-08 12:26:51 +1100

    add google tag and previous this was revamp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -129,6 +130,18 @@ export default function RootLayout({
       </head>
       <body className="antialiased">
         {children}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17712597201"
+        />
+        <Script id="gtag-init">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-17712597201');
+          `}
+        </Script>
       </body>
     </html>
   );
